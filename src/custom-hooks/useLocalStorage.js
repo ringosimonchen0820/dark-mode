e277fb19a8e745012da8145e2228952dae7b1dc4
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-13 16:07:20 -0500

    set up localStorage files

---
+++ src/custom-hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+//* set up state property
+//* capture the values
+//* if no data in localStorage, use initial data
+//* if there is data use that instead
+//* update localStorage
+//* Don't forget to parse data from localStorage, and convert new data to strings
+const useLocalStorage = (key, initialValue) => {
+	const [storedValue, setStoredValue] = useState(() => {
+		if (localStorage.getItem(key)) {
+			return JSON.parse(localStorage.getItem(key));
+		} else {
+			localStorage.setItem(key, JSON.stringify.initialValue);
+			return initialValue;
+		}
+	});
+	const setValue = newValue => {
+		setStoredValue(newValue);
+		localStorage.setItem(key, JSON.stringify.newValue);
+	};
+	return [storedValue, setValue];
+};
+
+export default useLocalStorage;
